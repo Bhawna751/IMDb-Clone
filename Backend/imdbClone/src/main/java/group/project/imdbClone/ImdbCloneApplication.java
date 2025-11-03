c135ac095db1255a5533381cc700d7ea0ca0d85e
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-11-03 18:27:59 +0530

    Project Initialization

---
+++ Backend/imdbClone/src/main/java/group/project/imdbClone/ImdbCloneApplication.java
@@ -0,0 +1,13 @@
+package group.project.imdbClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbCloneApplication.class, args);
+	}
+
+}
---
+++ Backend/imdbClone/src/test/java/group/project/imdbClone/ImdbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package group.project.imdbClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
