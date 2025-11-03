704d6769c9b6e5a084535b1a8f69cd9d5ddcff18
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-11-03 18:43:43 +0530

    first endpoint created successfully

--- Backend/imdbClone/src/main/java/group/project/imdbClone/ImdbCloneApplication.java
+++ Backend/imdbClone/src/main/java/group/project/imdbClone/ImdbCloneApplication.java
@@ -2,12 +2,17 @@ package group.project.imdbClone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ImdbCloneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImdbCloneApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(ImdbCloneApplication.class, args);}
 
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Hello World";
+	}
 }
