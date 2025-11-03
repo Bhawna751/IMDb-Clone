6a3a2b38f7d85f96d623124362b7e3d4fdd3ea97
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-11-03 18:54:53 +0530

    Database configuration

--- Backend/imdbClone/src/main/java/group/project/imdbClone/ImdbCloneApplication.java
+++ Backend/imdbClone/src/main/java/group/project/imdbClone/ImdbCloneApplication.java
@@ -2,17 +2,11 @@ package group.project.imdbClone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class ImdbCloneApplication {
 
 	public static void main(String[] args) {SpringApplication.run(ImdbCloneApplication.class, args);}
 
-	@GetMapping("/")
-	public String apiRoot(){
-		return "Hello World";
-	}
 }
