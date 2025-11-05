b79042f8e9c72d1df92bdf7088438c87da725b27
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-11-05 12:04:16 +0530

    Revert "axios cors error"
    This reverts commit ef1a858a65bd3ac1f6d3c71a2e17c295e8becbea.

--- Backend/imdbClone/src/main/java/group/project/imdbClone/MovieController.java
+++ Backend/imdbClone/src/main/java/group/project/imdbClone/MovieController.java
@@ -4,7 +4,10 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.text.html.Option;
 import java.util.List;
@@ -12,7 +15,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
-@CrossOrigin(origins = "*")
 public class MovieController {
 
     @Autowired
