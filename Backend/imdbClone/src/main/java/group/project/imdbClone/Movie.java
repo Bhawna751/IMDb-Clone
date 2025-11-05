82f7ded69f2e0f121859328aa4bdea02e37996b3
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-11-05 12:15:28 +0530

    removed lombok data annotation

--- Backend/imdbClone/src/main/java/group/project/imdbClone/Movie.java
+++ Backend/imdbClone/src/main/java/group/project/imdbClone/Movie.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
-@Data
+
 @Document(collection = "movies")
 @AllArgsConstructor
 @NoArgsConstructor
@@ -43,6 +43,75 @@ public class Movie {
 
     }
 
+    public ObjectId getId() {
+        return id;
+    }
 
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
 
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTrailerLink() {
+        return trailerLink;
+    }
+
+    public void setTrailerLink(String trailerLink) {
+        this.trailerLink = trailerLink;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public List<String> getBackdrops() {
+        return backdrops;
+    }
+
+    public void setBackdrops(List<String> backdrops) {
+        this.backdrops = backdrops;
+    }
+
+    public List<Review> getReviewIds() {
+        return reviewIds;
+    }
+
+    public void setReviewIds(List<Review> reviewIds) {
+        this.reviewIds = reviewIds;
+    }
 }
